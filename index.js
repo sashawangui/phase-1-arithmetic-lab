b0497e62d4fe006594b268a17e611df1730a1c86
sasha wangui <sasha.iluku@gmail.com>
2023-06-15 01:02:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.ceil(Math.random())
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(15, 18, 20);
